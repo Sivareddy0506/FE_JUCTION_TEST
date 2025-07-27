fee4cd71465f5a1403f25814c0b6995252407837
sravankumar <sravankumar@vassarlabs.com>
2025-07-27 12:16:26 +0530

    Updating address

--- lib/screens/profile/address/add_more_details.dart
+++ lib/screens/profile/address/add_more_details.dart
@@ -1,11 +1,22 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../../widgets/app_button.dart';
 import '../../../widgets/form_text.dart';
 import '../../../widgets/custom_appbar.dart';
+import 'package:http/http.dart' as http;
+
+import 'address.dart';
+
 
 class AddMoreDetailsPage extends StatefulWidget {
   final String address;
+  final String id;
+  final bool isEditable;
 
-  const AddMoreDetailsPage({super.key, required this.address});
+  const AddMoreDetailsPage(
+      {super.key,required this.id, required this.address, required this.isEditable});
 
   @override
   State<AddMoreDetailsPage> createState() => _AddMoreDetailsPageState();
@@ -14,6 +25,7 @@ class AddMoreDetailsPage extends StatefulWidget {
 class _AddMoreDetailsPageState extends State<AddMoreDetailsPage> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController addressController = TextEditingController();
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -44,6 +56,99 @@ class _AddMoreDetailsPageState extends State<AddMoreDetailsPage> {
           ],
         ),
       ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+        child: AppButton(
+          bottomSpacing: 0, // or remove it if handled by Padding
+          label: isLoading ? 'Sending...' : 'Submit',
+          backgroundColor: const Color(0xFF262626),
+          onPressed: _submit,
+        ),
+      ),
     );
+
   }
+
+  bool get isFormValid {
+    if (nameController.text.isEmpty || addressController.text.isEmpty) {
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> _submit() async {
+    if (!isFormValid) return;
+
+    setState(() => isLoading = true);
+    Uri uri;
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('authToken');
+    Map<String, dynamic> map = {};
+
+    if (widget.isEditable) {
+      uri = Uri.parse('https://api.junctionverse.com/user/update-address');
+      map = {
+        "addressId": widget.id,
+        "label": nameController.text,
+        "address": addressController.text,
+      };
+    } else {
+      uri = Uri.parse('https://api.junctionverse.com/user/addnew-address');
+      map = {
+        "label": nameController.text,
+        "address": addressController.text,
+        "setAsDefault": true,
+      };
+    }
+
+    try {
+      final headers = {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      };
+
+      final response = widget.isEditable
+          ? await http.put(uri, headers: headers, body: json.encode(map))
+          : await http.post(uri, headers: headers, body: json.encode(map));
+
+      if (response.statusCode == 200) {
+        // Show success message
+        String text = widget.isEditable
+            ? "Address update successfully"
+            : "Address added successfully";
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(text)),
+        );
+
+        int count = 0;
+
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (_) => const AddressPage()),
+              (Route<dynamic> route) {
+            return count++ >= 3; // keep the 3rd screen and above
+          },
+        );
+      } else {
+        // Show error from response body or fallback message
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Error: ${response.body.isNotEmpty
+                ? response.body
+                : 'Something went wrong'}'),
+          ),
+        );
+      }
+    } catch (e) {
+      if (!mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Something went wrong')),
+      );
+    } finally {
+      if (mounted) {
+        setState(() => isLoading = false);
+      }
+    }
+  }
+
 }
